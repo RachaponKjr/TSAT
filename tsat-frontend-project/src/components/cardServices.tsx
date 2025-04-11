bcf123c0e68643b2ee244e389e7ed86cc20078af
Nawakarn Leerattanachote <nawakarnleerattanachote@WV-MBP-009.local>
2025-04-11 21:35:00 +0700

    fix: card ratio

--- tsat-frontend-project/src/components/cardServices.tsx
+++ tsat-frontend-project/src/components/cardServices.tsx
@@ -6,13 +6,16 @@ export default function cardServices() {
     <div>
       <div className="w-[393px] mt-12 h-[285px] rounded-2xl bg-[#903035]">
         <div className="px-4 py-6 ">
-          <div className="text-white w-20 h-20">icon</div>
-          <div className="text-white text-xl font-bold">
+          <img src="../images/kilo-check.png" alt="Description of image" />
+          <div className="text-white mt-4 text-2xl font-bold">
             เช็คระยะ 10,000  กม.
           </div>
-          <div className="text-white">ดูแลเบื้องต้น เพิ่มความมั่นใจ </div>
+          <div className="text-xl mt-4">
+            ดูแลเบื้องต้น เพิ่มความมั่นใจ เหมาะสำหรับการดูแลประจำปี ให้รถขับลื่น
+            มั่นใจทุกการเดินทาง
+          </div>
         </div>
-      </div>{' '}
+      </div>
       <div className="grid grid-cols-2 gap-6 py-6">
         <SubCardServices />
         <SubCardServices />
--- tsat-frontend-project/src/components/subCardServices.tsx
+++ tsat-frontend-project/src/components/subCardServices.tsx
@@ -2,9 +2,15 @@ import React from 'react';
 
 export default function subCardServices() {
   return (
-    <div className="bg-[#903035] mx-auto w-full  rounded-lg  py-12">
-      <div className="text-center">1</div>
-      <div className="text-center">เครื่องยนต์</div>
+    <div className="bg-[#903035] mx-auto w-full rounded-lg py-6">
+      <div>
+        <img
+          src="../images/machine.png"
+          alt="Description of image"
+          className="mx-auto"
+        />
+      </div>
+      <div className="text-center text-xl text-white mt-4">เครื่องยนต์</div>
     </div>
   );
 }
